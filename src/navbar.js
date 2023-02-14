4ee3f819303bcdd40a74ce7efff154cd4b420893
Mr-Deee <merchantdaniel8@gmail.com>
2023-02-14 17:41:27 +0000

    initial

---
+++ src/navbar.js
@@ -0,0 +1,19 @@
+const Navbar = () => {
+    return (  
+<nav className="navbar">
+    <h1>
+        D|N
+    </h1>
+    <div className= 'links'>
+       <a href="/">Home</a> 
+       <a href="/create" style={{
+        
+       }}>Explore</a> 
+       <a href="/">Lofts</a>
+    </div>
+</nav>
+
+    );
+}
+ 
+export default Navbar;
