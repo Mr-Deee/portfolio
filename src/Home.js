3af32d5a7e94cd30fc8c5e9e4a63b0fcd7db2330
Mr-Deee <merchantdaniel8@gmail.com>
2023-02-15 08:14:50 +0000

    Initial

--- src/Home.js
+++ src/Home.js
@@ -1,1 +1,36 @@
 import { useState,useEffect } from "react";
+
+import BlogList from "./BlogList";
+
+const Home = () => {
+  const [blogs, setBlogs] = useState(null);
+  const [isPending, setIsPending] = useState(true);
+
+  // const handleDelete =(id)=>{
+  //   const newBlogs =blogs.filter(blog =>blog.id !==id);
+  //   setBlogs(newBlogs);
+  // }
+
+  useEffect(() => {
+    setTimeout(() => {
+      fetch(" http://localhost:8000/blogs")
+        .then((res) => {
+          return res.json();
+        })
+        .then((data) => {
+          setBlogs(data);
+          setIsPending(false);
+        });
+      // console.log(blogs)
+    });
+  }, []);
+
+  return (
+    <div className="container-div">
+      {isPending && <div>Loading....</div>}
+      {blogs && <BlogList blogs={blogs} title="ALL Blogs!" />}
+    </div>
+  );
+};
+
+export default Home;
