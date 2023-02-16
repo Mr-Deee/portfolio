d72097e98d72acf522dfe44f8ef21b70457d1e18
Mr-Deee <merchantdaniel8@gmail.com>
2023-02-16 14:59:56 +0000

    db.json working

--- src/Home.js
+++ src/Home.js
@@ -1,42 +1,21 @@
-import { useState,useEffect } from "react";
+
 
 import BlogList from "./BlogList";
+import useFetch from "./usefetch";
 
 const Home = () => {
-  const [blogs, setBlogs] = useState(null);
-  const [isPending, setIsPending] = useState(true);
-  const [error,setError]=useState(null);
-  // const handleDelete =(id)=>{
-  //   const newBlogs =blogs.filter(blog =>blog.id !==id);
-  //   setBlogs(newBlogs);
-  // }
-
-  useEffect(() => {
-    setTimeout(() => {
-      fetch('http://localhost:3000/blogs')
-        .then((res) => {
-          if(!res.ok){
-            throw Error("Could Not fetch")
-          }
-          return res.json();
-        })
-        .then((data) => {
-          setBlogs(data);
-          setIsPending(false);
-        }).catch(err=> { 
-          setIsPending(false);
-          setError(err.message);
-    });},
-
-   []);
+  const {data: blogs,isPending,error} = useFetch("http://localhost:3000/blogs");
+  
 
   return (
     <div className="container-div">
-      {error&& <div>could not fetch data </div>}
+      {error&& <div>{error}</div>}
       {isPending && <div>Loading....</div>}
       {blogs && <BlogList blogs={blogs} title="ALL Blogs!" />}
     </div>
   );
-});
+
 }
+
+
 export default Home;
