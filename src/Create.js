4519b186ab117b54850387b917e8d96f66459413
Mr-Deee <merchantdaniel8@gmail.com>
2023-02-23 12:20:31 +0000

    name of editor

--- src/Create.js
+++ src/Create.js
@@ -1,4 +1,14 @@
+import { useState } from "react";
+
+
 const Create = ()=>{
+
+
+
+    const [title, setTitle] = useState('');
+    const [body, setBody] = useState('');
+    const [author, setAuthor] = useState('');
+  
     return (
         <div className="create">
             <h2>Add a New  Blog</h2>
@@ -7,18 +17,29 @@ const Create = ()=>{
             <label>Blog title:</label>
             <input 
             type= "text"
-            required/>
+            required  
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+            
+            />
 
 
 <label>Blog body:</label>
             <textarea 
-             required><textarea/>
+             required
+             value={body}
+             onChange={(e) => setBody(e.target.value)}
+           />
+             <textarea/>
 
 
 <label>Blog author:</label>
             <select>
-                <option value = 'D'>D</option>
-                <option value = 'N'>N</option>
+                <option value = 'D|N'>D|N</option>
+                value={author}
+                onChange ={(e)=> setAuthor()}
+                {/* <option value = 'N'>|</option>
+                <option value = 'N'>N</option> */}
            
            </select>
            <button></button>
