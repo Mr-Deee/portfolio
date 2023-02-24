01ce4b8557b5309c4140174f7ca1d9d6432a2fae
Mr-Deee <merchantdaniel8@gmail.com>
2023-02-24 09:43:00 +0000

    creating a random button

--- src/Home.js
+++ src/Home.js
@@ -1,16 +1,49 @@
 import BlogList from "./BlogList";
 import useFetch from "./usefetch";
+const user = {
+  name: 'D|N',
+  imageUrl: 'https://i.imgur.com/yXOvdOSs.jpg',
+  imageSize: 90,
+};
+
+ function mybutton(){
+
+
+}
 
 const Home = () => {
   const {data: blogs,isPending,error} = useFetch("http://localhost:3000/blogs");
   
 
   return (
-    <div>
-      {error&& <div>{error}</div>}
-      {isPending && <div>Loading....</div>}
-      {blogs && <BlogList blogs={blogs}  />}
-    </div>
+
+    <><div className="WelcomeTxt">
+      
+      <h2>Sit-back relax and enjoy the Ride</h2>
+      
+      </div>
+      
+      
+      {/* <><h1>{user.name}</h1><img
+      className="avatar"
+      src={user.imageUrl}
+      alt={'Photo of ' + user.name}
+      style={{
+        width: user.imageSize,
+        height: user.imageSize
+      }} /></>
+      <div>
+        {error && <div>{error}</div>}
+        {isPending && <div>Loading....</div>}
+        {blogs && <BlogList blogs={blogs} />}
+      </div> */}
+      <div>
+        {error && <div>{error}</div>}
+        {isPending && <div>Loading....</div>}
+        {blogs && <BlogList blogs={blogs} />}
+      </div>
+      
+      </>
   );
 
 }
