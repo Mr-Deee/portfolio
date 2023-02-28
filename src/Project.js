5afeefeb875b4e27732f67acf211977d537e3ddb
Mr-Deee <merchantdaniel8@gmail.com>
2023-02-28 10:36:30 +0000

    removed button

--- src/Project.js
+++ src/Project.js
@@ -19,10 +19,22 @@ export default function MediaCard() {
   return (
 
     
-    
+    <div class='row'>
+
+      <div>
+        
+      </div>
+
+
+
+
+
+
+      
+      {/* Languages Pane */}
    
 
-    <Card sx={{ maxWidth: 345 }}>
+    <div class="column"><Card sx={{ maxWidth: 345 }}>
       <CardMedia
         sx={{ height: 240 }}
         image="https://th.bing.com/th/id/OIP.cevhDxOTXgO6M8gMsM-1aAHaIb?pid=ImgDet&rs=1"
@@ -33,15 +45,33 @@ export default function MediaCard() {
           Flutter
         </Typography>
        
-          Lizards are a widespread group of squamate reptiles, with over 6,000
-          species, ranging across all continents except Antarctica
+  
         
       </CardContent>
      
-    </Card>
+    </Card></div>
 
 
+  {/* React */}
 
+  <div class="column"> <Card sx={{ maxWidth: 345 }}>
+      <CardMedia
+        sx={{ height: 240 }}
+        image="https://www.google.com/url?sa=i&url=https%3A%2F%2Fionicframework.com%2Fdocs%2Freact&psig=AOvVaw1R-tEU3gcjmqN8_23N_7oU&ust=1677662694097000&source=images&cd=vfe&ved=0CBAQjRxqFwoTCJDAytnyt_0CFQAAAAAdAAAAABAR"
+        title="React Logo"
+      />
+      <CardContent>
+        <Typography gutterBottom variant="h5" component="div">
+          React
+        </Typography>
+       
+        
+        
+      </CardContent>
+     
+    </Card></div>
+
+    </div>
 
   );
 
