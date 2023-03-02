7498343d4e05a1efc2be452ef6ae1fb52938a16e
Mr-Deee <merchantdaniel8@gmail.com>
2023-03-02 13:10:26 +0000

    bootstrap added

--- src/Home.js
+++ src/Home.js
@@ -1,26 +1,10 @@
-// import BlogList from "./BlogList";
-// import useFetch from "./usefetch";
 import Particle from "./Particle";
-// import { bounce } from "react-animations";
-// import styled from "styled-components";
+
 import { Container, Row, Col } from "react-bootstrap";
-// import { keyframes } from "@emotion/react";
 import Type from "./Type";
-// import { blue } from "@mui/material/colors";
-
-
-
-
-
-// const Bounce = styled.div`animation:  2s ${keyframes`${bounce}`} infinite;`;
-
 
 function Home() {
-//  const {data: blogs,isPending,error} = useFetch("http://localhost:3000/blogs");
-  
-
   return (
-
     <section>
       <Container fluid className="home-section" id="home">
         <Particle />
@@ -47,10 +31,7 @@ function Home() {
         </Container>
       </Container>
     </section>
-
-
-);
+  );
 }
-
 
 export default Home;
