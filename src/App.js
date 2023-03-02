2e0fdea2c3c9b3e88ffb56c78d0774eb33736974
Mr-Deee <merchantdaniel8@gmail.com>
2023-03-02 11:14:17 +0000

    style added nav bar problem logo footer

--- src/App.js
+++ src/App.js
@@ -2,10 +2,17 @@ import React, { useState, useEffect } from "react";
 import Navbar from "./Navbar";
 import Preloader from "./Pre";
 import Home from "./Home";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+// import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import "./App.css";
+// import ScrollToTop from ".ScrollToTop";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Navigate
+} from "react-router-dom";
 // import Create from "./Create";
-import BlogDetails from "./BlogDetails"
+import ScrollToTop from "./ScrollToTop";
 import Projects from "./Project";
 import "./style.css";
 import "./App.css";
@@ -23,26 +30,47 @@ const [load, upadateLoad] = useState(true);
   }, []);
 
   return (
-    <Router>
-      <div className="App">
-        <Navbar />
-        <div className="content">
-          <Switch>
-            <Route exact path="/">
-              <Home />
-            </Route>
-            <Route path="/project">
-              <Projects />
-            </Route>
 
-            <Route path="/blogs/:id">
-              <BlogDetails/>
-            </Route>
-          </Switch>
-        </div>
-      </div>
-    </Router>
-  );
+    <Router>
+    <Preloader load={load} />
+    <div className="App" id={load ? "no-scroll" : "scroll"}>
+      <Navbar />
+    <ScrollToTop />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/project" element = {<Projects />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/" element={<Home />} />
+        <Route path="*" element={<Navigate to="/"/>} />
+      </Routes>
+      {/* <Footer /> */}
+    </div>
+  </Router>
+);
+
+
+
+
+    // <Router>
+    //   <div className="App">
+    //     <Navbar />
+    //     <div className="content">
+    //       <Switch>
+    //         <Route exact path="/">
+    //           <Home />
+    //         </Route>
+    //         <Route path="/project">
+    //           <Projects />
+    //         </Route>
+
+    //         <Route path="/blogs/:id">
+    //           <BlogDetails/>
+    //         </Route>
+    //       </Switch>
+    //     </div>
+    //   </div>
+    // </Router>
+  //);
 }
 
 export default App;
