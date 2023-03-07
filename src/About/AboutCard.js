9b07587a666d395695625beebda843ee14f8b510
Mr-Deee <merchantdaniel8@gmail.com>
2023-03-07 17:05:08 +0000

    techstack change

--- src/About/AboutCard.js
+++ src/About/AboutCard.js
@@ -8,10 +8,9 @@ function AboutCard() {
       <Card.Body>
         <blockquote className="blockquote mb-0">
           <p style={{ textAlign: "justify" }}>
-            Hi Everyone, I am <span className="purple">Soumyajit Behera </span>
-            from <span className="purple"> Bhubaneswar, India.</span>
-            <br />I am a junior pursuing IMSC in Maths and Computing in BIT
-            Mesra.
+            Hi Everyone, I am <span className="purple">Daniel Narterh </span>
+            from <span className="purple"> Accra, Ghana.</span>
+            <br />I am a graduate from Ghana Communication Technology University with a BSc certficate in Information Technology.
             <br />
             <br />
             Apart from coding, some other activities that I love to do!
@@ -21,10 +20,10 @@ function AboutCard() {
               <ImPointRight /> Playing Games
             </li>
             <li className="about-activity">
-              <ImPointRight /> Writting Tech Blogs
+              <ImPointRight /> coding
             </li>
             <li className="about-activity">
-              <ImPointRight /> Travelling
+              <ImPointRight /> Learning0
             </li>
           </ul>
 
